d39de45109292c008df7fc929ca2d3cd0b2a8982
Stephen Yu <jiaxun.s.yu@gmail.com>
2013-02-28 21:30:01 -0800

    Implement JCA encryption/decryption.

---
+++ test/org/lpgreen/misc/AESTests.java
@@ -0,0 +1,110 @@
+package org.lpgreen.misc;
+
+import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+
+/**
+ * It is the SecurityManager junit testing class. 
+ * 
+ * Creation date: Feb. 23, 2013
+ * Last modify date: Feb. 23, 2013
+ * 
+ * @author  J Stephen Yu
+ * @version 1.0
+ */
+@SuppressWarnings("deprecation")
+public class AESTests extends AbstractTransactionalDataSourceSpringContextTests {
+
+	public void testAESEncryption() {
+		System.out.println("Test --> testAESEncryption");
+		try {
+			LPCipher lpCipher = new LPCipher();
+			String clearText;
+			String encryptText;
+			String decryptText;
+			byte[] encryptBytes;
+
+			clearText = "Encryption Test 1 - Customer John Smith";
+			System.out.println("    Case 1 - encrypt to byte[]");
+			encryptBytes = lpCipher.encryptToBytes(clearText);
+			decryptText = lpCipher.decryptFromBytes(encryptBytes);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptBytes);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			System.out.println("    Case 1 - encrypt to hex string");
+			encryptText = lpCipher.encrypt(clearText);
+			decryptText = lpCipher.decrypt(encryptText);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptText);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			clearText = "Encryption Test 2 - John Smith's Credit Card: 4321987634125556";
+			System.out.println("    Case 2 - encrypt to byte[]");
+			encryptBytes = lpCipher.encryptToBytes(clearText);
+			decryptText = lpCipher.decryptFromBytes(encryptBytes);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptBytes);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			System.out.println("    Case 2 - encrypt to hex string");
+			encryptText = lpCipher.encrypt(clearText);
+			decryptText = lpCipher.decrypt(encryptText);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptText);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			clearText = "Encryption Test 3 - John Smith's bank account: 121-546-3655502";
+			System.out.println("    Case 3 - encrypt to byte[]");
+			encryptBytes = lpCipher.encryptToBytes(clearText);
+			decryptText = lpCipher.decryptFromBytes(encryptBytes);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptBytes);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			System.out.println("    Case 3 - encrypt to hex string");
+			encryptText = lpCipher.encrypt(clearText);
+			decryptText = lpCipher.decrypt(encryptText);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptText);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			clearText = "Encryption Test 4 - random  t e  x   t 122 @#1 *^&1";
+			System.out.println("    Case 4 - encrypt to byte[]");
+			encryptBytes = lpCipher.encryptToBytes(clearText);
+			decryptText = lpCipher.decryptFromBytes(encryptBytes);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptBytes);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			System.out.println("    Case 4 - encrypt to hex string");
+			encryptText = lpCipher.encrypt(clearText);
+			decryptText = lpCipher.decrypt(encryptText);
+			System.out.println("    Clear  : " + clearText);
+			System.out.println("    Encrypt: " + encryptText);
+			System.out.println("    Decrypt: " + decryptText);
+			assertEquals(clearText, decryptText);
+			System.out.println("    PASS");
+
+			System.out.println("     <-- Done.");
+		}
+		catch (Exception e) {
+			System.out.println("Exception");
+			fail(e.getMessage());
+		}
+	}
+
+}
---
+++ test/org/lpgreen/misc/Blowfish.java
@@ -0,0 +1,125 @@
+package org.lpgreen.misc;
+
+import java.security.SecureRandom;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * It is the cipher class encapsulate the Blowfish encryption/decryption. 
+ * 
+ * Creation date: Feb. 28, 2013
+ * Last modify date: Feb. 28, 2013
+ * 
+ * @author  J Stephen Yu
+ * @version 1.0
+ */
+public class Blowfish {
+
+	// Blowfish Encryption/Decryption
+	SecretKeySpec keySpec;
+	Cipher        cipher;
+
+	public Blowfish() {
+		try {
+			// get the Blowfish key generator
+			KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
+			if (keyGen == null) {
+				throw new Exception("No Blowfish provider");
+			}
+			// random number generator
+			SecureRandom sRand = SecureRandom.getInstance("SHA1PRNG");
+			if (sRand == null) {
+				throw new Exception("No SHA1PRNG provider");
+			}
+
+			String seed = "LPGREENSend1!#";
+			sRand.setSeed(seed.getBytes());
+			keyGen.init(128, sRand);
+
+	        SecretKey key = keyGen.generateKey();
+			keySpec = new SecretKeySpec(key.getEncoded(), "Blowfish");   
+
+			// get the Blowfish engine class
+			cipher = Cipher.getInstance("Blowfish");
+			if (cipher == null) {
+				throw new Exception("No Blowfish cipher");
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Blowfish constructor Exception: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Encrypt the input clear text to a byte array 
+	 * 
+	 * @param clearText: input clear text
+	 * @return byte array of cipher text
+	 */
+    public byte[] encryptToBytes(String clearText) throws Exception {
+    	try {
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			return cipher.doFinal(clearText.getBytes());
+    	}
+    	catch (Exception e) {
+			System.out.println("Blowfish encryptToBytes exception: " + e.getMessage());
+			return null;
+    	}
+    }
+
+	/**
+	 * Decrypt the input cipher bytes to a clear text 
+	 * 
+	 * @param encryptBytes: input cipher bytes
+	 * @return clear text
+	 */
+    public String decryptFromBytes(byte[] encryptBytes) throws Exception {
+    	try {
+			cipher.init(Cipher.DECRYPT_MODE, keySpec);
+			return new String(cipher.doFinal(encryptBytes));
+    	}
+		catch (Exception e) {
+			System.out.println("Blowfish decryptFromBytes exception: " + e.getMessage());
+			return null;
+		}
+    }
+
+	/**
+	 * Encrypt the input clear text to a hex cipher text 
+	 * 
+	 * @param clearText: input clear text
+	 * @return hex cipher text
+	 */
+    public String encrypt(String clearText) throws Exception {
+    	try {
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			return Hex.encodeHexString(cipher.doFinal(clearText.getBytes()));
+    	}
+    	catch (Exception e) {
+			System.out.println("Blowfish encrypt exception: " + e.getMessage());
+			return null;
+    	}
+    }
+
+	/**
+	 * Decrypt the input hex cipher text to a clear text 
+	 * 
+	 * @param encryptText: input hex cipher text
+	 * @return clear text
+	 */
+    public String decrypt(String encryptText) throws Exception {
+    	try {
+			cipher.init(Cipher.DECRYPT_MODE, keySpec);
+			return new String(cipher.doFinal(DatatypeConverter.parseHexBinary(encryptText)));
+    	}
+		catch (Exception e) {
+			System.out.println("Blowfish decrypt exception: " + e.getMessage());
+			return null;
+		}
+    }
+
+}
