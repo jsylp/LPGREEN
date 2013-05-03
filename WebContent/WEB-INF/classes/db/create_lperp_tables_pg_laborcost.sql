08a1a872648771f6853d0fbb20f71f09f2966278
Stephen Yu <jiaxun.s.yu@gmail.com>
2013-05-03 06:18:34 -0700

    Update DB script.

--- WebContent/WEB-INF/classes/db/create_lperp_tables_pg_laborcost.sql
+++ WebContent/WEB-INF/classes/db/create_lperp_tables_pg_laborcost.sql
@@ -13,11 +13,14 @@ CREATE TABLE LaborCostMethod (
 	CostMethodType          varchar(30) NOT NULL,
 	Description             varchar(255),
 	UnitQuantity            decimal(19,4),
+	UnitQuantityCost        decimal(19,4),
 	UnitQuantityUMCode      varchar(20),
+	UserDefinedUMCode       varchar(20),
+	Multiplier              decimal(10,4),
 	RangeStartQuantity      int,
 	RangeEndQuantity        int,
-	StartWeekDayTime        char(8),
-	Duration                char(5),
+	OTStartWeekdayTime      char(8),
+	OTDuration              char(5),
 	OwnerId                 uuid,
 	OwnerAccountId          int REFERENCES Account(Id),
 	CreatedDate             timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
@@ -30,27 +33,66 @@ CREATE UNIQUE INDEX LCM_CODE_indx ON LaborCostMethod(OwnerAccountId, upper(CostM
 ------------------------------------------------------------------------------
 -- Load LaborCostMethod
 ------------------------------------------------------------------------------
-INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description, UnitQuantity,
-		UMCode, MinQuantity, MaxQuantity, StartTime, Duration, OwnerAccountId)
-	VALUES('Exempt', 'PerMonth', 'Salaried Employee', 1.0,
-	       'Month', 6, 12, '01-08:00', '08:00', 1);
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0001', 'PerDuration', 'Monthly full time employee salary',
+	       1.0, null, 'Month', null,
+	       null, null, null, null, null, 1);
 
-INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description, UnitQuantity,
-		UMCode, MinQuantity, MaxQuantity, StartTime, Duration, OwnerAccountId)
-	VALUES('NonExempt', 'PerHour', 'Consultant', 1.0,
-	       'Month', 6, 12, '01-08:00', '08:00', 1);
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0002', 'PerDuration', 'Bi monthly full time employee salary',
+	       0.5, null, 'Month', null,
+	       null, null, null, null, null, 1);
 
-INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description, UnitQuantity,
-		UMCode, MinQuantity, MaxQuantity, StartTime, Duration, OwnerAccountId)
-	VALUES('Exempt', 'PerMonth', 'Salaried Employee', 1.0,
-	       'Month', 6, 12, '01-08:00', '08:00', 1);
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0003', 'PerDuration', 'Weekly full time employee salary',
+	       1.0, null, 'Week', null,
+	       null, null, null, null, null, 1);
 
-INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description, UnitQuantity,
-		UMCode, MinQuantity, MaxQuantity, StartTime, Duration, OwnerAccountId)
-	VALUES('Exempt', 'PerMonth', 'Salaried Employee', 1.0,
-	       'Month', 6, 12, '01-08:00', '08:00', 1);
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0004', 'PerTask', 'Task for setup test lab',
+	       1.0, 15000.00, null, 'task',
+	       null, null, null, null, null, 1);
 
-INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description, UnitQuantity,
-		UMCode, MinQuantity, MaxQuantity, StartTime, Duration, OwnerAccountId)
-	VALUES('Exempt', 'PerMonth', 'Salaried Employee', 1.0,
-	       'Month', 6, 12, '01-08:00', '08:00', 1);
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0005', 'PerTask', 'Task for setup computers',
+	       1.0, 6750.00, null, 'task',
+	       null, null, null, null, null, 1);
+
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0006', 'PerQuantity', 'Assemble 100 notebook computers',
+	       100.0, 25000.00, null, 'piece',
+	       null, null, null, null, null, 1);
+
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0007', 'PerQuantity', 'Make 10000 gallon gas',
+	       10000.0, 15000.00, 'gallon', null,
+	       null, null, null, null, null, 1);
+
+INSERT INTO LaborCostMethod(CostMethodCode, CostMethodType, Description,
+	       UnitQuantity, UnitQuantityCost, UnitQuantityUMCode, UserDefinedUMCode,
+	       Multiplier, RangeStartQuantity, RangeEndQuantity,
+	       OTStartWeekDayTime, OTDuration, OwnerAccountId)
+	VALUES('CMC-0008', 'PerQuantityWithTier', 'Make 10000 gallon gas',
+	       10000.0, 15000.00, 'gallon', null,
+	       null, null, null, null, null, 1);
